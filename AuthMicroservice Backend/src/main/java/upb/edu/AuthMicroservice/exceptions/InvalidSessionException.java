864299494eda56ac188d271d4f24d0946e84c6ca
theTarnished <alex4750384@gmail.com>
2025-08-29 21:20:53 -0400

    fix: merge conflicts

--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/exceptions/InvalidSessionException.java
+++
@@ -1,5 +0,0 @@
-package upb.edu.AuthMicroservice.exceptions;
-
-public class InvalidSessionException extends RuntimeException {
-    public InvalidSessionException(String message) { super(message); }
-}
--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/repositories/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package upb.edu.AuthMicroservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import upb.edu.AuthMicroservice.models.UserProfile;
-
-@Repository
-public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
-}
